6ca1eff063f4db334071cb3803ae7e753a9a0e00
Paulina Stefanska <paulina@airframe.io>
2020-11-08 13:11:29 +0100

    Added useful packages and initial settings

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,17 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app id="inspire">
+    <v-container column text-center>
+      <div class="nav">
+        <v-btn to="/" text color="warning">Home</v-btn>
+        <v-btn to="/users" text color="warning">Users</v-btn>
+      </div>
+      <router-view/>
+    </v-container>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
